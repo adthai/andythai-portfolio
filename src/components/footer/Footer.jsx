5848e55b126226dfddf3887b00b03d837587926b
adthai <adthai@uw.edu>
2024-07-30 14:58:26 -0700

    removing some content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -29,7 +29,6 @@ const Footer = () => {
         <div className={css.right}>
           <ul className={css.menu}>
             <li><a href="#expertise">About Me</a></li>
-            {/* <li><a href="#work">Experience</a></li> */}
             <li><a href="#projects">Projects</a></li>
             <li>
               <SocialIcon network="linkedin" url="https://www.linkedin.com/in/adthai01" />
